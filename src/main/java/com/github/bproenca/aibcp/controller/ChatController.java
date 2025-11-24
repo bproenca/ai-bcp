f4a84a1d5afdc9ce8b0f22dc1a90e0045f478de6
Bruno Proenca <bruno.proenca@synchro.com.br>
2025-11-24 12:28:22 -0300

    openai integration

--- src/main/java/com/github/bproenca/aibcp/controller/ChatController.java
+++ src/main/java/com/github/bproenca/aibcp/controller/ChatController.java
@@ -2,8 +2,10 @@ package com.github.bproenca.aibcp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -12,7 +14,18 @@ import java.time.LocalDateTime;
 @RequestMapping("/api")
 public class ChatController {
 
+    private final ChatClient chatClient;
     private static final Logger log = LoggerFactory.getLogger(ChatController.class);
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message) {
+        log.info("Chat for message {}", message);
+        return chatClient.prompt(message).call().content();
+    }
 
     @GetMapping("/ping")
     public String chat() {
